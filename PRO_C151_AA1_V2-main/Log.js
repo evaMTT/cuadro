0ba37409ba8f182f368f8e5dc7f0a22ee23bc231
evaMTT <104604358+evaMTT@users.noreply.github.com>
2023-11-06 19:51:06 -0600

    Add files via upload

---
+++ PRO_C151_AA1_V2-main/Log.js
@@ -0,0 +1,10 @@
+// Registrando el componente en log-component.js
+AFRAME.registerComponent('log', {
+    schema: {
+      message: {type: 'string', default: '¡Hola, mundo!'}
+    },
+    init: function () {
+        console.log(this.data.message);
+      }
+  });
+
